f6cc1b27705d4a83a29afc866a28e1d8f9a71a17
FlyHippo <whoarewe4u@protonmail.com>
2024-04-16 15:14:11 +0800

    异常处理+校验

--- blog-backend/src/main/java/com/flyinghippo/personalblogbackend/entity/Result.java
+++ blog-backend/src/main/java/com/flyinghippo/personalblogbackend/entity/Result.java
@@ -5,36 +5,44 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-public class Result implements Serializable {
-    private String code;
+public class Result<T> implements Serializable {
+    private int code;      //200表示正常，非200表示异常
     private String msg;
-    private Object data;
-    public static Result succ(Object data) {
+    private T data;
+
+    public static<T> Result succ() {
+        return succ(200,"操作成功",null);
+    }
+    public static<T> Result succ(T data) {
+        return succ(200,"操作成功",data);
+    }
+    public static<T> Result succ(String msg, T data) {
+        return succ(200,msg,data);
+    }
+    public static<T> Result succ(int code,String msg){
+        return succ(200,msg,null);
+    }
+
+    public static<T> Result succ(int code,String msg, T data) {
         Result m = new Result();
-        m.setCode("0");
+        m.setCode(200);
         m.setData(data);
-        m.setMsg("操作成功");
+        m.setMsg(msg);
         return m;
     }
-    public static Result succ(String mess, Object data) {
-        Result m = new Result();
-        m.setCode("0");
-        m.setData(data);
-        m.setMsg(mess);
-        return m;
+
+    public static<T> Result fail(String msg){
+        return fail(400,msg,null);
+
     }
-    public static Result fail(String mess) {
-        Result m = new Result();
-        m.setCode("-1");
-        m.setData(null);
-        m.setMsg(mess);
-        return m;
+    public static<T> Result fail(String msg,T data) {
+        return fail(400,msg,data);
     }
-    public static Result fail(String mess, Object data) {
+    public static<T> Result fail(int code,String msg, T data) {
         Result m = new Result();
-        m.setCode("-1");
+        m.setCode(400);
         m.setData(data);
-        m.setMsg(mess);
+        m.setMsg(msg);
         return m;
     }
 }
--- blog-backend/src/main/java/com/flyinghippo/personalblogbackend/entity/User.java
+++ blog-backend/src/main/java/com/flyinghippo/personalblogbackend/entity/User.java
@@ -9,6 +9,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * <p>
  * 
@@ -21,6 +24,7 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("m_user")
+//@ApiModel("用户实体")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -28,10 +32,14 @@ public class User implements Serializable {
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
+    @NotBlank(message = "昵称不能为空")
     private String username;
 
     private String avatar;
+    //头像
 
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message =    "邮箱格式不正确")
     private String email;
 
     private String password;
