6d706e2dd3bd344d05c1593c4649c18f897e9862
FlyHippo <whoarewe4u@protonmail.com>
2024-04-16 15:50:28 +0800

    fix: cors

--- blog-backend/src/main/java/com/flyinghippo/personalblogbackend/config/CorsConfig.java
+++ blog-backend/src/main/java/com/flyinghippo/personalblogbackend/config/CorsConfig.java
@@ -1,6 +1,10 @@
 package com.flyinghippo.personalblogbackend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,13 +13,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .maxAge(3600)
-                .allowedHeaders("*");
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //允许任何域名访问
+        corsConfiguration.addAllowedOrigin("*");
+        //允许任何header访问
+        corsConfiguration.addAllowedHeader("*");
+        //允许任何方法访问
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setMaxAge(3600L);         // 预检请求的有效期，单位为秒。
+        corsConfiguration.setAllowCredentials(true);// 是否支持安全证书(必需参数)
+        return corsConfiguration;
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
 }
